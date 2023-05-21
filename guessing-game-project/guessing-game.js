6b4df6a4fc33b0382a60b6cd6026ee9fa4d95da0
Srivatsan <vatsan1993@gmail.com>
2023-05-21 11:27:32 +0530

    finished askGuess function

--- guessing-game-project/guessing-game.js
+++ guessing-game-project/guessing-game.js
@@ -1,6 +1,14 @@
+// importing readline
+const readline = require('node:readline');
+// creating interface to read  and write.
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
 let secretNumber = 10;
 
-let checkGuess = (guess) => {
+const checkGuess = (guess) => {
   if (guess > secretNumber) {
     console.log('Too high');
     return false;
@@ -13,10 +21,20 @@ let checkGuess = (guess) => {
   }
 };
 
-console.log(checkGuess(30));
-console.log(checkGuess(20));
-console.log(checkGuess(15));
-console.log(checkGuess(1));
-console.log(checkGuess(3));
-console.log(checkGuess(5));
-console.log(checkGuess(10));
+const askGuess = () => {
+  rl.question('Enter a guess: ', (answer) => {
+    answer = Number(answer);
+    checkGuess(answer);
+    rl.close();
+  });
+};
+
+// console.log(checkGuess(30));
+// console.log(checkGuess(20));
+// console.log(checkGuess(15));
+// console.log(checkGuess(1));
+// console.log(checkGuess(3));
+// console.log(checkGuess(5));
+// console.log(checkGuess(10));
+
+askGuess();
