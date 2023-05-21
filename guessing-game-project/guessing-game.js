6ad9064198e862d96248178031c94ad8c73973ee
Srivatsan <vatsan1993@gmail.com>
2023-05-21 12:08:59 +0530

    finished askRange

--- guessing-game-project/guessing-game.js
+++ guessing-game-project/guessing-game.js
@@ -1,18 +1,18 @@
-// // importing readline
-// const readline = require('node:readline');
-// // creating interface to read  and write.
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-// });
-
-let secretNumber = 10;
+// importing readline
+const readline = require('node:readline');
+// creating interface to read  and write.
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 const randomInRange = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+let secretNumber = randomInRange(0, 100);
 
 const checkGuess = (guess) => {
   if (guess > secretNumber) {
@@ -34,6 +34,16 @@ const askGuess = () => {
     rl.close();
   });
 };
+// Enter a max number: *20*
+// Enter a min number: *11*
+// I'm thinking of a number between 11 and 20...
+const askRange = () => {
+  rl.question('Enter a max number: ', (max) => {
+    rl.question('Enter a min number: ', (min) => {
+      console.log(`I'm thinking of a number between ${min} and ${max}...`);
+    });
+  });
+};
 
 // console.log(checkGuess(30));
 // console.log(checkGuess(20));
@@ -43,8 +53,10 @@ const askGuess = () => {
 // console.log(checkGuess(5));
 // console.log(checkGuess(10));
 
+// console.log(randomInRange(5, 10));
+// console.log(randomInRange(4, 6));
+// console.log(randomInRange(5, 7));
+// console.log(randomInRange(5, 8));
+
 // askGuess();
-console.log(randomInRange(5, 10));
-console.log(randomInRange(4, 6));
-console.log(randomInRange(5, 7));
-console.log(randomInRange(5, 8));
+askRange();
